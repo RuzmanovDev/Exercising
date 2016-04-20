9f8d07f8dec7f9fec30237cba68e61f6f7748ffd
RuzmanovDev <stoyan.ruzmanov@gmail.com>
2016-04-20 14:05:26 +0300

    Added StudentsSystem win form app

---
+++ StudentWindowsApp/StudentSystem/MainForm.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StudentSystem
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void добавянеToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            AddFormOfEdu f2 = new AddFormOfEdu();
+            f2.ShowDialog();
+        }
+    }
+}
