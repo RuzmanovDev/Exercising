522ed047cd033014af8c2f9a1d3d51252f00944f
RuzmanovDev <stoyan.ruzmanov@gmail.com>
2016-05-14 12:29:55 +0300

    Tested cascade edit

--- MassiveDynamic/MassiveDynamic/DeleteCustomesForm.Designer.cs
+++ MassiveDynamic/MassiveDynamic/DeleteCustomesForm.Designer.cs
@@ -36,6 +36,7 @@
             this.customersBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.customersTableAdapter = new MassiveDynamic.MassiveDynamicDataSetTableAdapters.CustomersTableAdapter();
             this.deleteCustomersButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.customersBindingSource1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.massiveDynamicDataSetBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.massiveDynamicDataSet)).BeginInit();
@@ -47,7 +48,7 @@
             this.deleteComboBox.DataSource = this.customersBindingSource1;
             this.deleteComboBox.DisplayMember = "CustomerID";
             this.deleteComboBox.FormattingEnabled = true;
-            this.deleteComboBox.Location = new System.Drawing.Point(28, 23);
+            this.deleteComboBox.Location = new System.Drawing.Point(95, 29);
             this.deleteComboBox.Name = "deleteComboBox";
             this.deleteComboBox.Size = new System.Drawing.Size(121, 21);
             this.deleteComboBox.TabIndex = 0;
@@ -79,7 +80,7 @@
             // 
             // deleteCustomersButton
             // 
-            this.deleteCustomersButton.Location = new System.Drawing.Point(183, 23);
+            this.deleteCustomersButton.Location = new System.Drawing.Point(238, 29);
             this.deleteCustomersButton.Name = "deleteCustomersButton";
             this.deleteCustomersButton.Size = new System.Drawing.Size(75, 23);
             this.deleteCustomersButton.TabIndex = 1;
@@ -87,11 +88,21 @@
             this.deleteCustomersButton.UseVisualStyleBackColor = true;
             this.deleteCustomersButton.Click += new System.EventHandler(this.deleteCustomersButton_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "ID на клиента";
+            // 
             // DeleteCustomesForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(315, 77);
+            this.ClientSize = new System.Drawing.Size(373, 82);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.deleteCustomersButton);
             this.Controls.Add(this.deleteComboBox);
             this.Name = "DeleteCustomesForm";
@@ -102,6 +113,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.massiveDynamicDataSet)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.customersBindingSource)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -114,5 +126,6 @@
         private MassiveDynamicDataSetTableAdapters.CustomersTableAdapter customersTableAdapter;
         private System.Windows.Forms.BindingSource customersBindingSource1;
         private System.Windows.Forms.Button deleteCustomersButton;
+        private System.Windows.Forms.Label label1;
     }
 }
