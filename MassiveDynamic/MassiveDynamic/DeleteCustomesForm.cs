4b4c68f140c7b16422d97280cb5852de23299adf
RuzmanovDev <stoyan.ruzmanov@gmail.com>
2016-05-14 12:54:47 +0300

    pochvame da majem po master detail

--- MassiveDynamic/MassiveDynamic/DeleteCustomesForm.cs
+++ MassiveDynamic/MassiveDynamic/DeleteCustomesForm.cs
@@ -1,17 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Configuration;
-using System.Data;
-using System.Data.SqlClient;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
-namespace MassiveDynamic
+﻿namespace MassiveDynamic
 {
+    using System;
+    using System.Configuration;
+    using System.Data.SqlClient;
+    using System.Windows.Forms;
+
     public partial class DeleteCustomesForm : Form
     {
         public DeleteCustomesForm()
@@ -40,7 +33,7 @@ namespace MassiveDynamic
             {
                 con.Open();
                 com.ExecuteNonQuery();
-                MessageBox.Show("Данните са изтрити успешно!","Message/Съобщение");
+                MessageBox.Show("Данните са изтрити успешно!", "Message/Съобщение");
             }
             catch (Exception)
             {
