d7b305ee7accea0ce978af348cb362dbc510cfcc
RuzmanovDev <stoyan.ruzmanov@gmail.com>
2016-09-05 12:33:36 +0300

    Added linked list written in JS

---
+++ LogAn/LogAnalyzerTests/LogAnalyzerTests.cs
@@ -0,0 +1,33 @@
+﻿namespace LogAnTests
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class LogAnalyzerTests
+    {
+        [TestCase("filewithgoodextension.SLF")]
+        [TestCase("filewithgoodextension.slf")]
+        public void
+ IsValidLogFileName_ValidExtensions_ReturnsTrue(string file)
+        {
+            LogAnalyzer la = MakeAnalyzer();
+            bool result = la.IsValidLogFileName(file);
+            Assert.True(result);
+        }
+
+
+        [Test]
+        public void
+IsValidFileName_WhenCalled_ChangesWasLastFileNameValid()
+        {
+            LogAnalyzer la = MakeAnalyzer();
+            la.IsValidLogFileName("badname.foo");
+            Assert.False(la.WasLastFileNameValid);
+        }
+
+        private LogAnalyzer MakeAnalyzer()
+        {
+            return new LogAnalyzer();
+        }
+    }
+}
